d83a6510d3d08a4a0b44dd757a9762c43f82d2cf
Scott Hurrey <scott.hurrey@gmail.com>
2023-07-24 13:01:42 -0400

    Initial Commit

---
+++ build/scripts/Box-AI-API-SDK-Java.bat
@@ -0,0 +1,92 @@
+@rem
+@rem Copyright 2015 the original author or authors.
+@rem
+@rem Licensed under the Apache License, Version 2.0 (the "License");
+@rem you may not use this file except in compliance with the License.
+@rem You may obtain a copy of the License at
+@rem
+@rem      https://www.apache.org/licenses/LICENSE-2.0
+@rem
+@rem Unless required by applicable law or agreed to in writing, software
+@rem distributed under the License is distributed on an "AS IS" BASIS,
+@rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+@rem See the License for the specific language governing permissions and
+@rem limitations under the License.
+@rem
+
+@if "%DEBUG%"=="" @echo off
+@rem ##########################################################################
+@rem
+@rem  Box-AI-API-SDK-Java startup script for Windows
+@rem
+@rem ##########################################################################
+
+@rem Set local scope for the variables with windows NT shell
+if "%OS%"=="Windows_NT" setlocal
+
+set DIRNAME=%~dp0
+if "%DIRNAME%"=="" set DIRNAME=.
+@rem This is normally unused
+set APP_BASE_NAME=%~n0
+set APP_HOME=%DIRNAME%..
+
+@rem Resolve any "." and ".." in APP_HOME to make it shorter.
+for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
+
+@rem Add default JVM options here. You can also use JAVA_OPTS and BOX_AI_API_SDK_JAVA_OPTS to pass JVM options to this script.
+set DEFAULT_JVM_OPTS=
+
+@rem Find java.exe
+if defined JAVA_HOME goto findJavaFromJavaHome
+
+set JAVA_EXE=java.exe
+%JAVA_EXE% -version >NUL 2>&1
+if %ERRORLEVEL% equ 0 goto execute
+
+echo.
+echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
+echo.
+echo Please set the JAVA_HOME variable in your environment to match the
+echo location of your Java installation.
+
+goto fail
+
+:findJavaFromJavaHome
+set JAVA_HOME=%JAVA_HOME:"=%
+set JAVA_EXE=%JAVA_HOME%/bin/java.exe
+
+if exist "%JAVA_EXE%" goto execute
+
+echo.
+echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
+echo.
+echo Please set the JAVA_HOME variable in your environment to match the
+echo location of your Java installation.
+
+goto fail
+
+:execute
+@rem Setup the command line
+
+set CLASSPATH=%APP_HOME%\lib\Box-AI-API-SDK-Java.jar;%APP_HOME%\lib\box-java-sdk-4.2.0.jar;%APP_HOME%\lib\slf4j-simple-2.0.7.jar;%APP_HOME%\lib\jackson-annotations-2.15.2.jar;%APP_HOME%\lib\jackson-core-2.15.2.jar;%APP_HOME%\lib\jackson-databind-2.15.2.jar;%APP_HOME%\lib\commons-cli-1.5.0.jar;%APP_HOME%\lib\json-20230618.jar;%APP_HOME%\lib\spring-web-6.0.11.jar;%APP_HOME%\lib\spring-beans-6.0.11.jar;%APP_HOME%\lib\spring-core-6.0.11.jar;%APP_HOME%\lib\javax.servlet-api-4.0.1.jar;%APP_HOME%\lib\cache-api-1.1.1.jar;%APP_HOME%\lib\httpclient-4.5.14.jar;%APP_HOME%\lib\httpclient5-5.2.1.jar;%APP_HOME%\lib\guava-31.1-jre.jar;%APP_HOME%\lib\minimal-json-0.9.5.jar;%APP_HOME%\lib\jose4j-0.9.0.jar;%APP_HOME%\lib\bcpkix-jdk15on-1.57.jar;%APP_HOME%\lib\bcprov-jdk15on-1.57.jar;%APP_HOME%\lib\okhttp-4.10.0.jar;%APP_HOME%\lib\groovy-testng-3.0.15.jar;%APP_HOME%\lib\testng-7.5.jar;%APP_HOME%\lib\slf4j-api-2.0.7.jar;%APP_HOME%\lib\spring-jcl-6.0.11.jar;%APP_HOME%\lib\micrometer-observation-1.10.9.jar;%APP_HOME%\lib\httpcore-4.4.16.jar;%APP_HOME%\lib\commons-logging-1.2.jar;%APP_HOME%\lib\commons-codec-1.11.jar;%APP_HOME%\lib\httpcore5-h2-5.2.jar;%APP_HOME%\lib\httpcore5-5.2.jar;%APP_HOME%\lib\groovy-ant-3.0.15.jar;%APP_HOME%\lib\groovy-astbuilder-3.0.15.jar;%APP_HOME%\lib\groovy-cli-picocli-3.0.15.jar;%APP_HOME%\lib\groovy-groovysh-3.0.15.jar;%APP_HOME%\lib\groovy-console-3.0.15.jar;%APP_HOME%\lib\groovy-datetime-3.0.15.jar;%APP_HOME%\lib\groovy-groovydoc-3.0.15.jar;%APP_HOME%\lib\groovy-docgenerator-3.0.15.jar;%APP_HOME%\lib\groovy-jmx-3.0.15.jar;%APP_HOME%\lib\groovy-json-3.0.15.jar;%APP_HOME%\lib\groovy-jsr223-3.0.15.jar;%APP_HOME%\lib\groovy-macro-3.0.15.jar;%APP_HOME%\lib\groovy-nio-3.0.15.jar;%APP_HOME%\lib\groovy-servlet-3.0.15.jar;%APP_HOME%\lib\groovy-sql-3.0.15.jar;%APP_HOME%\lib\groovy-swing-3.0.15.jar;%APP_HOME%\lib\groovy-templates-3.0.15.jar;%APP_HOME%\lib\groovy-test-3.0.15.jar;%APP_HOME%\lib\groovy-test-junit5-3.0.15.jar;%APP_HOME%\lib\groovy-xml-3.0.15.jar;%APP_HOME%\lib\groovy-3.0.15.jar;%APP_HOME%\lib\failureaccess-1.0.1.jar;%APP_HOME%\lib\listenablefuture-9999.0-empty-to-avoid-conflict-with-guava.jar;%APP_HOME%\lib\jsr305-3.0.2.jar;%APP_HOME%\lib\checker-qual-3.12.0.jar;%APP_HOME%\lib\error_prone_annotations-2.11.0.jar;%APP_HOME%\lib\j2objc-annotations-1.3.jar;%APP_HOME%\lib\okio-jvm-3.0.0.jar;%APP_HOME%\lib\kotlin-stdlib-jdk8-1.5.31.jar;%APP_HOME%\lib\kotlin-stdlib-jdk7-1.5.31.jar;%APP_HOME%\lib\kotlin-stdlib-1.6.20.jar;%APP_HOME%\lib\micrometer-commons-1.10.9.jar;%APP_HOME%\lib\ant-junit-1.10.12.jar;%APP_HOME%\lib\ant-1.10.12.jar;%APP_HOME%\lib\ant-launcher-1.10.12.jar;%APP_HOME%\lib\ant-antlr-1.10.12.jar;%APP_HOME%\lib\picocli-4.6.3.jar;%APP_HOME%\lib\qdox-1.12.1.jar;%APP_HOME%\lib\javaparser-core-3.24.2.jar;%APP_HOME%\lib\jline-2.14.6.jar;%APP_HOME%\lib\junit-4.13.2.jar;%APP_HOME%\lib\junit-platform-launcher-1.9.1.jar;%APP_HOME%\lib\junit-platform-engine-1.9.1.jar;%APP_HOME%\lib\junit-platform-commons-1.9.1.jar;%APP_HOME%\lib\junit-jupiter-engine-5.9.1.jar;%APP_HOME%\lib\junit-jupiter-api-5.9.1.jar;%APP_HOME%\lib\kotlin-stdlib-common-1.6.20.jar;%APP_HOME%\lib\annotations-13.0.jar;%APP_HOME%\lib\hamcrest-core-1.3.jar;%APP_HOME%\lib\opentest4j-1.2.0.jar;%APP_HOME%\lib\jcommander-1.78.jar;%APP_HOME%\lib\jquery-3.5.1.jar
+
+
+@rem Execute Box-AI-API-SDK-Java
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %BOX_AI_API_SDK_JAVA_OPTS%  -classpath "%CLASSPATH%" boxdev.ai.BoxAiDemo %*
+
+:end
+@rem End local scope for the variables with windows NT shell
+if %ERRORLEVEL% equ 0 goto mainEnd
+
+:fail
+rem Set variable BOX_AI_API_SDK_JAVA_EXIT_CONSOLE if you need the _script_ return code instead of
+rem the _cmd.exe /c_ return code!
+set EXIT_CODE=%ERRORLEVEL%
+if %EXIT_CODE% equ 0 set EXIT_CODE=1
+if not ""=="%BOX_AI_API_SDK_JAVA_EXIT_CONSOLE%" exit %EXIT_CODE%
+exit /b %EXIT_CODE%
+
+:mainEnd
+if "%OS%"=="Windows_NT" endlocal
+
+:omega
